f912f59eed09c0933aa57ae1cc258699b4082c01
GAURAV1107 <gauravsingh1107@gmail.com>
2019-08-18 12:50:08 +0530

    added framework

---
+++ lib/utils/create_driver.py
@@ -0,0 +1,22 @@
+import unittest
+import pytest
+from selenium.webdriver import Chrome,Firefox
+
+def get_driver_instance():
+    browser_type = pytest.config.option.browser.lower()
+    os_name = pytest.config.option.system.lower()
+    url_info = pytest.config.option.url.lower()
+    if os_name=="windows":
+        if browser_type=="chrome":
+            driver=Chrome('./browser_server/chromedriver.exe')
+        elif browser_type=="firefox":
+            driver= Firefox('./browser_server/chromedriver.exe')
+
+    driver.maximize_window()
+    driver.implicitly_wait(30)
+    if url_info=="test":
+        driver.get("//http://localhost")
+    elif url_info=="prod":
+        driver.get("https://freecrm.com/")
+
+    return driver
